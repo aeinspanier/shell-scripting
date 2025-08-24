5619c842f0a36cd0a69d5824f633882fed16f5c5
aeinspanier <72575207+aeinspanier@users.noreply.github.com>
2025-08-24 10:03:55 -0500

    adding operators.sh

---
+++ operators.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+PINEAPPLE=50
+BANANA=4
+WATERMEON=20
+BASKET=2
+
+TOTAL=$(($PINEAPPLE + $BANANA + $WATERMEON))
+
+echo "ITEMS IN BASKET:"
+echo "Pineapples: $PINEAPPLE"
+echo "Bananas: $BANANA"
+echo "Watermeons: $WATERMEON"
+echo "Baskets: $BASKET"
+echo "---------------------"
+echo "Total fruits: $TOTAL"
